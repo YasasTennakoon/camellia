4a1c86f7591c228b371d83e5f938fd7f7c24686a
Yasas Tennakoon <85722525+YasasTennakoon@users.noreply.github.com>
2025-10-25 06:51:17 +0000

    add

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -44,6 +44,11 @@ export default function Home() {
                         <a
                             href="#contact"
                             className="inline-flex items-center justify-center rounded-md border border-white/40 px-5 py-2.5 text-sm font-semibold text-white/90 hover:bg-white/10 transition"
+                            onClick={() =>
+                                window.open(
+                                    "https://www.booking.com/hotel/lk/sesatha-lake-kandy.en-gb.html?aid=2311236&label=en-lk-booking-desktop-IncWB4CtfrbYgbu3xC6gmwS652796017434%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-334108349%3Alp9069790%3Ali%3Adec%3Adm&sid=b00b4883fc8e6538f6c9efd4a83b9b47&all_sr_blocks=1119284203_395031659_0_2_0%2C1119284204_395031659_2_2_0&checkin=2025-10-25&checkout=2025-10-26&dist=0&group_adults=3&group_children=0&hapos=3&highlighted_blocks=1119284203_395031659_0_2_0%2C1119284204_395031659_2_2_0&hpos=3&matching_block_id=1119284203_395031659_0_2_0&no_rooms=1&req_adults=3&req_children=0&room1=A%2CA%2CA&sb_price_type=total&sr_order=distance_from_search&sr_pri_blocks=1119284203_395031659_0_2_0__3150%2C1119284204_395031659_2_2_0__3500&srepoch=1761374743&srpvid=661c2f838f750135&type=total&ucfs=1&",
+                                    "_blank"
+                                )}
                         >
                             Check Availability
                         </a>
--- src/pages/Rooms/components/RoomDetailsPopover.tsx
+++ src/pages/Rooms/components/RoomDetailsPopover.tsx
@@ -1,5 +1,6 @@
 import type { RoomDetalisProps } from "./RoomCard";
 import { FaWifi, FaParking, FaSnowflake, FaConciergeBell, FaTv, FaStar } from "react-icons/fa";
+import { RoomImageSlider } from "./RoomImageSlider";
 
 type RoomDetailsPopoverProps = {
     roomDetails: RoomDetalisProps;
@@ -16,11 +17,7 @@ export const RoomDetailsPopover = ({ roomDetails, popoverStatus }: RoomDetailsPo
         "
             >
                 <div className="md:w-1/2 w-full h-64 md:h-auto relative">
-                    <img
-                        src={roomDetails.images?.[0]}
-                        alt={roomDetails.title}
-                        className="absolute inset-0 w-full h-full object-cover"
-                    />
+                    <RoomImageSlider images={roomDetails.images} />
                 </div>
 
                 <div className="md:w-1/2 w-full flex flex-col justify-between bg-white px-6 py-6 md:py-10">
@@ -78,6 +75,11 @@ export const RoomDetailsPopover = ({ roomDetails, popoverStatus }: RoomDetailsPo
                 hover:bg-[#8d42c4] transition-colors duration-200
                 text-sm md:text-sm tracking-wide
               "
+                            onClick={() =>
+                                window.open(
+                                    "https://www.booking.com/hotel/lk/sesatha-lake-kandy.en-gb.html?aid=2311236&label=en-lk-booking-desktop-IncWB4CtfrbYgbu3xC6gmwS652796017434%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-334108349%3Alp9069790%3Ali%3Adec%3Adm&sid=b00b4883fc8e6538f6c9efd4a83b9b47&all_sr_blocks=1119284203_395031659_0_2_0%2C1119284204_395031659_2_2_0&checkin=2025-10-25&checkout=2025-10-26&dist=0&group_adults=3&group_children=0&hapos=3&highlighted_blocks=1119284203_395031659_0_2_0%2C1119284204_395031659_2_2_0&hpos=3&matching_block_id=1119284203_395031659_0_2_0&no_rooms=1&req_adults=3&req_children=0&room1=A%2CA%2CA&sb_price_type=total&sr_order=distance_from_search&sr_pri_blocks=1119284203_395031659_0_2_0__3150%2C1119284204_395031659_2_2_0__3500&srepoch=1761374743&srpvid=661c2f838f750135&type=total&ucfs=1&",
+                                    "_blank"
+                                )}
                         >
                             BOOK NOW
                         </button>
